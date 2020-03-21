1b1f8d4ec89d001f561d9404844ba67471777519
qhdwight <qhdwight@gmail.com>
2020-03-21 13:02:09 -0700

    Refactoring

--- Assets/Scripts/ArmIk.cs
+++ Assets/Scripts/ArmIk.cs
@@ -30,23 +30,26 @@ public class ArmIk : MonoBehaviour
         return true;
     }
 
-    private static void HandleArm(Arm arm)
+    private static void PositionArm(Arm arm)
     {
-        Transform target = arm.target;    
-        if (!SetupArm(arm) || target == null) return; 
+        Transform target = arm.target;
+        if (!SetupArm(arm) || target == null) return;
         Transform[] bones = arm.bones;
+        Vector3 targetPosition = target.position;
         float rightForearmLength = Vector3.Distance(bones[HandIndex].position, bones[ForearmIndex].position),
               rightUpperArmLength = Vector3.Distance(bones[ForearmIndex].position, bones[UpperArmIndex].position),
-              lengthToTarget = Vector3.Distance(target.position, bones[UpperArmIndex].position);
+              lengthToTarget = Vector3.Distance(targetPosition, bones[UpperArmIndex].position);
         {
             Transform upperArm = bones[UpperArmIndex];
-            Vector3 direction = target.position - upperArm.position;
-            float z = Mathf.Rad2Deg * Mathf.Atan2(-direction.x, -direction.z);
-            float x = GetAngleFromTriangle(rightUpperArmLength, lengthToTarget, rightForearmLength);
-            // x += Mathf.Rad2Deg * Mathf.Atan2(-direction.y, direction.z);
-            x += Mathf.Rad2Deg * Mathf.Atan2(-direction.y, Vector3.Distance(upperArm.position, new Vector3 {x = target.position.x, y = upperArm.position.y, z = target.position.z}));
-            if (!double.IsNaN(x) && !double.IsNaN(z))
-                upperArm.localRotation = Quaternion.Euler(0.0f, 0.0f, z) * Quaternion.Euler(360.0f - x, 0.0f, 0.0f);
+            Vector3 upperArmPosition = upperArm.position,
+                    directionToTarget = targetPosition - upperArmPosition;
+            float lateralAngleToTarget = Mathf.Rad2Deg * Mathf.Atan2(-directionToTarget.x, -directionToTarget.z),
+                  lateralDistanceToTarget = Vector3.Distance(upperArmPosition, new Vector3 {x = targetPosition.x, y = upperArmPosition.y, z = targetPosition.z}),
+                  elevationAngleToTarget = Mathf.Rad2Deg * Mathf.Atan2(-directionToTarget.y, lateralDistanceToTarget),
+                  upperArmAngle = GetAngleFromTriangle(rightUpperArmLength, lengthToTarget, rightForearmLength) + elevationAngleToTarget;
+            if (!double.IsNaN(upperArmAngle) && !double.IsNaN(lateralAngleToTarget))
+                // Order is important - first look towards target in xz plane and then rotate
+                upperArm.localRotation = Quaternion.Euler(0.0f, 0.0f, lateralAngleToTarget) * Quaternion.Euler(360.0f - upperArmAngle, 0.0f, 0.0f);
         }
         {
             Transform forearm = bones[ForearmIndex];
@@ -59,12 +62,13 @@ public class ArmIk : MonoBehaviour
 
     private void LateUpdate()
     {
-        HandleArm(m_Left);
-        HandleArm(m_Right);
+        PositionArm(m_Left);
+        PositionArm(m_Right);
     }
 
     private static float GetAngleFromTriangle(float a, float b, float c)
     {
+        // Law of cosines to find angle given three known side lengths
         return Mathf.Rad2Deg * Mathf.Acos((Mathf.Pow(a, 2) + Mathf.Pow(b, 2) - Mathf.Pow(c, 2)) / (2 * a * b));
     }
 }
