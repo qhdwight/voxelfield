30aa211d6c286daa064fc36a80db2cb66cd67e1b
qhdwight <qhdwight@gmail.com>
2020-07-30 15:32:23 -0700

    Geeng gong

--- Assets/Scripts/Voxels/VoxelChange.cs
+++ Assets/Scripts/Voxels/VoxelChange.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
-using Swihoni.Util.Math;
+﻿using Swihoni.Util.Math;
 using UnityEngine;
 
 namespace Voxels
@@ -13,49 +11,45 @@ namespace Voxels
         Wall,
         Prism
     }
-
+    
     public struct VoxelChange
     {
+        public readonly struct Key
+        {
+            public readonly Position3Int origin;
+            public readonly VoxelVolumeForm form;
+            // Spherical, Cylindrical, Wall
+            public readonly float magnitude;
+            // Prism
+            public readonly Position3Int upperBound;
+
+            public bool Equals(in Key other) => origin.Equals(other.origin) && form == other.form && magnitude.Equals(other.magnitude) && upperBound.Equals(other.upperBound);
+
+            public override bool Equals(object other) => other is Key otherKey && Equals(otherKey);
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    int hashCode = origin.GetHashCode();
+                    hashCode = (hashCode * 397) ^ (int) form;
+                    hashCode = (hashCode * 397) ^ magnitude.GetHashCode();
+                    hashCode = (hashCode * 397) ^ upperBound.GetHashCode();
+                    return hashCode;
+                }
+            }
+
+            public static bool operator ==(in Key left, in Key right) => left.Equals(right);
+            public static bool operator !=(in Key left, in Key right) => !(left == right);
+        }
+        
         public byte? texture, density, orientation;
         public bool? hasBlock, isBreakable, natural, replace, modifiesBlocks, noRandom;
         public Color32? color;
-        public float? magnitude, yaw;
-        public VoxelVolumeForm? form;
-        public Position3Int? upperBound;
-        
+
         public override string ToString() =>
-            $"{nameof(texture)}: {texture}, {nameof(density)}: {density}, {nameof(orientation)}: {orientation}, {nameof(hasBlock)}: {hasBlock}, {nameof(isBreakable)}: {isBreakable}, {nameof(natural)}: {natural}, {nameof(replace)}: {replace}, {nameof(modifiesBlocks)}: {modifiesBlocks}, {nameof(noRandom)}: {noRandom}, {nameof(color)}: {color}, {nameof(magnitude)}: {magnitude}, {nameof(yaw)}: {yaw}, {nameof(form)}: {form}, {nameof(upperBound)}: {upperBound}";
-
-        private bool Equals(in VoxelChange other) => texture == other.texture && density == other.density && orientation == other.orientation && hasBlock == other.hasBlock
-                                                  && isBreakable == other.isBreakable && natural == other.natural && replace == other.replace
-                                                  && modifiesBlocks == other.modifiesBlocks && noRandom == other.noRandom
-                                                  && Nullable.Equals(color, other.color) && Nullable.Equals(magnitude, other.magnitude)
-                                                  && Nullable.Equals(yaw, other.yaw) && form == other.form && Nullable.Equals(upperBound, other.upperBound);
-
-        public override bool Equals(object other) => other is VoxelChange otherChange && Equals(otherChange);
-
-        [SuppressMessage("ReSharper", "NonReadonlyMemberInGetHashCode")]
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                int hashCode = texture.GetHashCode();
-                hashCode = (hashCode * 397) ^ density.GetHashCode();
-                hashCode = (hashCode * 397) ^ orientation.GetHashCode();
-                hashCode = (hashCode * 397) ^ hasBlock.GetHashCode();
-                hashCode = (hashCode * 397) ^ isBreakable.GetHashCode();
-                hashCode = (hashCode * 397) ^ natural.GetHashCode();
-                hashCode = (hashCode * 397) ^ replace.GetHashCode();
-                hashCode = (hashCode * 397) ^ modifiesBlocks.GetHashCode();
-                hashCode = (hashCode * 397) ^ noRandom.GetHashCode();
-                hashCode = (hashCode * 397) ^ color.GetHashCode();
-                hashCode = (hashCode * 397) ^ magnitude.GetHashCode();
-                hashCode = (hashCode * 397) ^ yaw.GetHashCode();
-                hashCode = (hashCode * 397) ^ form.GetHashCode();
-                hashCode = (hashCode * 397) ^ upperBound.GetHashCode();
-                return hashCode;
-            }
-        }
+            $"{nameof(texture)}: {texture}, {nameof(density)}: {density}, {nameof(orientation)}: {orientation}, {nameof(hasBlock)}: {hasBlock}, {nameof(isBreakable)}: {isBreakable}, {nameof(natural)}: {natural}, {nameof(replace)}: {replace}, {nameof(modifiesBlocks)}: {modifiesBlocks}, {nameof(noRandom)}: {noRandom}, {nameof(color)}: {color}";
+        
 
         public void Merge(in VoxelChange newChange)
         {
@@ -69,10 +63,6 @@ namespace Voxels
             if (newChange.modifiesBlocks.HasValue) modifiesBlocks = newChange.modifiesBlocks.Value;
             if (newChange.noRandom.HasValue) noRandom = newChange.noRandom.Value;
             if (newChange.color.HasValue) color = newChange.color.Value;
-            if (newChange.magnitude.HasValue) magnitude = newChange.magnitude.Value;
-            if (newChange.yaw.HasValue) yaw = newChange.yaw.Value;
-            if (newChange.form.HasValue) form = newChange.form.Value;
-            if (newChange.upperBound.HasValue) upperBound = newChange.upperBound.Value;
         }
     }
 }
